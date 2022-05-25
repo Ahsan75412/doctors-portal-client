c1904cd53af85a355756c4ffa336406468037364
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-25 13:03:50 +0600

    added banner , card and services section here !

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+import ServiceBanner from './ServiceBanner';
+import Services from './Services';
+
+
+const Home = () => {
+    return (
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <ServiceBanner></ServiceBanner>
+        </div>
+    );
+};
+
+export default Home;
